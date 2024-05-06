07c22001a90b8ea432875ab755f8ea75d0ece257
Ridham Patel <127187827+ridh21@users.noreply.github.com>
2024-05-06 09:14:20 +0530

    Added Starter Pack

---
+++ starter-pack/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-full bg-slate-900 h-screen flex items-center justify-center">
+      <div className="w-[50%] rounded-xl h-52 bg-slate-800 text-white flex flex-col items-center justify-center">
+        <h1 className="text-bold text-3xl">Vite React Tailwind Starter Pack</h1>
+        <a href="https://github.com/ridh21">
+          <h2 className="text-semibold ">by <span className="underline">ridh21</span> </h2>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
